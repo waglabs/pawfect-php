35428a1f3e3f7179244ca9c0dcad5b0c47b567f9
Andrew Breksa <andrew.breksa@wagwalking.com>
2021-02-11 10:54:03 -0800

    [misc] add new Relationships helper for use assertions

---
+++ src/Assertions/Relationships.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of waglabs/pawfect-php.
+ *
+ * (C) 2021 Wag Labs, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace WagLabs\PawfectPHP\Assertions;
+
+
+use WagLabs\PawfectPHP\ReflectionClass;
+
+/**
+ * Trait Relationships
+ * @package WagLabs\PawfectPHP\Assertions
+ * @author  Andrew Breksa <andrew.breksa@wagwalking.com>
+ */
+trait Relationships
+{
+
+    /**
+     * @param ReflectionClass $reflectionClass
+     * @param string          $class
+     * @return bool
+     */
+    public function dependsOn(ReflectionClass $reflectionClass, string $class): bool
+    {
+        return in_array($class, $reflectionClass->getUses());
+    }
+
+}
---
+++ tests/Assertions/RelationshipsTest.php
@@ -0,0 +1,118 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of waglabs/pawfect-php.
+ *
+ * (C) 2021 Wag Labs, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Assertions;
+
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use WagLabs\PawfectPHP\AbstractRule;
+use WagLabs\PawfectPHP\Assertions\Relationships;
+use WagLabs\PawfectPHP\FileLoader\FileLoaderInterface;
+use WagLabs\PawfectPHP\ReflectionClass;
+use WagLabs\PawfectPHP\Results;
+
+/**
+ * Class RelationshipsTest
+ * @package Assertions
+ * @author  Andrew Breksa <andrew.breksa@wagwalking.com>
+ */
+class RelationshipsTest extends TestCase
+{
+
+    public function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+
+    public function testDependOnFalse()
+    {
+        $reflectionClass = Mockery::mock(ReflectionClass::class);
+        $reflectionClass->shouldReceive('getUses')
+            ->andReturn([
+                Results::class,
+                ReflectionClass::class
+            ]);
+        $rule = new class extends AbstractRule {
+
+            use Relationships;
+
+            public function supports(ReflectionClass $reflectionClass): bool
+            {
+                return false;
+            }
+
+            public function execute(ReflectionClass $reflectionClass)
+            {
+                return;
+            }
+
+            public function getName(): string
+            {
+                return 'test-rule';
+            }
+
+            public function getDescription(): string
+            {
+                return 'test description';
+            }
+        };
+
+        self::assertFalse($rule->dependsOn($reflectionClass, FileLoaderInterface::class));
+    }
+
+    public function testDependsOnTrue()
+    {
+        $reflectionClass = Mockery::mock(ReflectionClass::class);
+        $reflectionClass->shouldReceive('getUses')
+            ->andReturn([
+                Results::class,
+                ReflectionClass::class
+            ]);
+        $rule = new class extends AbstractRule {
+
+            use Relationships;
+
+            public function supports(ReflectionClass $reflectionClass): bool
+            {
+                return false;
+            }
+
+            public function execute(ReflectionClass $reflectionClass)
+            {
+                return;
+            }
+
+            public function getName(): string
+            {
+                return 'test-rule';
+            }
+
+            public function getDescription(): string
+            {
+                return 'test description';
+            }
+        };
+
+        self::assertTrue($rule->dependsOn($reflectionClass, ReflectionClass::class));
+    }
+
+}
